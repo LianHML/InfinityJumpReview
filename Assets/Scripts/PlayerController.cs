b03c1d42b75a6d74a067dad6fec08b8626f4c283
Lian <lianlinsmeyer@gmail.com>
2022-08-16 20:52:28 -0300

    Implementação PlayerController
    Movimentação do jogador.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
+    private float speed = 6;
+    private float jumpForce = 10;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float direction = Input.GetAxis("Horizontal") * Time.deltaTime;
+            player.transform.Translate(direction * speed, 0f, 0f);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Platform"))
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, jumpForce);
+        }
     }
 }
