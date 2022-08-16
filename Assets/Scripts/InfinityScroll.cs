77b195d508c8809af58539281a71c8bc93e8ca28
Lian <lianlinsmeyer@gmail.com>
2022-08-16 14:45:47 -0300

    Implementação InfinityScroll
    Programação da rolagem infinita de plataformas.

--- Assets/Scripts/InfinityScroll.cs
+++ Assets/Scripts/InfinityScroll.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class InfinityScroll : MonoBehaviour
 {
+    private GameObject[] platform;
+    private float width = 6f;
+    private Vector2 speed;
+    private Vector2 randomWidth;
+
     void Start()
     {
+        platform = GameObject.FindGameObjectsWithTag("Platform");
 
+        speed = new Vector2(0, -2);
+
+        randomWidth = new Vector2(-5.5f, 5.5f);
     }
 
     void Update()
     {
-
+        InstantiatePlatform();
     }
 
+    void InstantiatePlatform()
+    {
+        foreach (GameObject platformScroll in platform)
+        {
+            platformScroll.transform.Translate(speed * Time.deltaTime);
+
+            if (platformScroll.transform.position.y < width * -1)
+            {
+                platformScroll.transform.Translate(new Vector2(0, width * 2));
+                RandomPlatformPosition(platformScroll);
+            }
+        }
+    }
+
+    private void RandomPlatformPosition(GameObject platform)
+    {
+        float randomValue = Random.Range(randomWidth.x, randomWidth.y);
+        platform.transform.position = new Vector2(randomValue, platform.transform.position.y);
+    }
 }
